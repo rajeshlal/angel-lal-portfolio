94b727a98b6420bfb7dc9e71f1bbaa9af067a182
Rajesh Lal <rajeshlal07@gmail.com>
2026-07-06 15:49:40 -0700

    Render bento cards from content data array

--- js/script.js
+++ js/script.js
@@ -1,4 +1,27 @@
 // js/script.js
 (function () {
-  console.log("script.js loaded, CONTENT has", CONTENT.length, "items");
+  const grid = document.getElementById("grid");
+
+  function cardMarkup(item) {
+    const media = item.thumb
+      ? `<img class="card__thumb" src="${item.thumb}" alt="${item.title}">`
+      : `<div class="card__placeholder"><span>${item.title}</span></div>`;
+
+    return `
+      <article class="card card--${item.size}" data-id="${item.id}" tabindex="0" role="button" aria-label="Open ${item.title}">
+        <span class="card__type card__type--${item.type}">${item.type}</span>
+        ${media}
+        <div class="card__caption">
+          <h3 class="card__title">${item.title}</h3>
+          <p class="card__excerpt">${item.excerpt}</p>
+        </div>
+      </article>
+    `;
+  }
+
+  function renderGrid() {
+    grid.innerHTML = CONTENT.map(cardMarkup).join("");
+  }
+
+  renderGrid();
 })();
